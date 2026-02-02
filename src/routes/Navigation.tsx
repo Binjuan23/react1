e59f2d702aabc240226c817fa5b14510efba3faa
j.brotons.martinez <j.brotons.martinez@accenture.com>
2026-02-02 13:01:46 +0100

    Lazy components

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -3,6 +3,8 @@ import {Navigate, NavLink, NavLinkRenderProps} from 'react-router-dom';
 import logo from '../logo.svg';
 import {createBrowserRouter, Outlet, RouterProvider} from "react-router";
 import {withFaroRouterInstrumentation} from "@grafana/faro-react";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
 
 const MainLayout = () => {
     const activeNavStyle = ({isActive}: NavLinkRenderProps) => isActive ? 'nav-active' : '';
@@ -12,13 +14,13 @@ const MainLayout = () => {
                 <img src={logo} alt="React Logo"/>
                 <ul>
                     <li>
-                        <NavLink to="home" className={activeNavStyle}>Home</NavLink>
+                        <NavLink to="lazy1" className={activeNavStyle}>Lazy 1</NavLink>
                     </li>
                     <li>
-                        <NavLink to="about" className={activeNavStyle}>About</NavLink>
+                        <NavLink to="lazy2" className={activeNavStyle}>Lazy 2</NavLink>
                     </li>
                     <li>
-                        <NavLink to="users" className={activeNavStyle}>Users</NavLink>
+                        <NavLink to="lazy3" className={activeNavStyle}>Lazy 3</NavLink>
                     </li>
                 </ul>
             </nav>
@@ -32,10 +34,10 @@ const routes = [
         path: '/',
         element: <MainLayout/>,
         children: [
-            {path: 'home', element: <h1>Home Page</h1>},
-            {path: 'about', element: <h1>About Page</h1>},
-            {path: 'users', element: <h1>Users Page</h1>},
-            {path: '*', element: <Navigate to="/home" replace/>},
+            {path: 'lazy1', element: <LazyPage1/>},
+            {path: 'lazy2', element: <LazyPage2/>},
+            {path: 'lazy3', element: <LazyPage3/>},
+            {path: '*', element: <Navigate to="/lazy1" replace/>},
         ],
     },
 ];
