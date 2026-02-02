bb844cdb83ee100ff75016e19db0c7a603900b92
j.brotons.martinez <j.brotons.martinez@accenture.com>
2026-02-02 14:37:59 +0100

    Lazyload basic done

--- src/index.tsx
+++ src/index.tsx
@@ -3,47 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {TracingInstrumentation} from "@grafana/faro-web-tracing";
-import {
-    createReactRouterV6DataOptions,
-    getWebInstrumentations,
-    initializeFaro,
-    ReactIntegration
-} from "@grafana/faro-react";
-import {matchRoutes} from "react-router-dom";
 
-initializeFaro({
-    url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/63885dabd1504de68fea585052ef6120',
-    app: {
-        name: 'Mine',
-        version: '1.0.0',
-        environment: 'production'
-    },
-
-    instrumentations: [
-        // Mandatory, omits default instrumentations otherwise.
-        ...getWebInstrumentations(),
-
-        // Tracing package to get end-to-end visibility for HTTP requests.
-        new TracingInstrumentation(),
-
-        //React integration for React applications.
-        // new ReactIntegration({
-        //     router: createReactRouterV6Options({
-        //         createRoutesFromChildren,
-        //         matchRoutes,
-        //         Routes,
-        //         useLocation,
-        //         useNavigationType,
-        //     }),
-        // }),
-        new ReactIntegration({
-            router: createReactRouterV6DataOptions({
-                matchRoutes,
-            }),
-        }),
-    ],
-});
+// initializeFaro({
+//     url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/63885dabd1504de68fea585052ef6120',
+//     app: {
+//         name: 'Mine',
+//         version: '1.0.0',
+//         environment: 'production'
+//     },
+//
+//     instrumentations: [
+//         // Mandatory, omits default instrumentations otherwise.
+//         ...getWebInstrumentations(),
+//
+//         // Tracing package to get end-to-end visibility for HTTP requests.
+//         new TracingInstrumentation(),
+//
+//         //React integration for React applications.
+//         // new ReactIntegration({
+//         //     router: createReactRouterV6Options({
+//         //         createRoutesFromChildren,
+//         //         matchRoutes,
+//         //         Routes,
+//         //         useLocation,
+//         //         useNavigationType,
+//         //     }),
+//         // }),
+//         new ReactIntegration({
+//             router: createReactRouterV6DataOptions({
+//                 matchRoutes,
+//             }),
+//         }),
+//     ],
+// });
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,82 +1,80 @@
 import {Navigate, NavLink, NavLinkRenderProps} from 'react-router-dom';
-
+import {Suspense} from 'react';
 import logo from '../logo.svg';
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router";
-import {withFaroRouterInstrumentation} from "@grafana/faro-react";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import {BrowserRouter, Route, Routes} from "react-router";
+import {routes} from "./routes";
 
+// const MainLayout = () => {
+//     const activeNavStyle = ({isActive}: NavLinkRenderProps) => isActive ? 'nav-active' : '';
+//     return (
+//         <div className="main-layout">
+//             <nav>
+//                 <img src={logo} alt="React Logo"/>
+//                 <ul>
+//                     <li>
+//                         <NavLink to="lazy1" className={activeNavStyle}>Lazy 1</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink to="lazy2" className={activeNavStyle}>Lazy 2</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink to="lazy3" className={activeNavStyle}>Lazy 3</NavLink>
+//                     </li>
+//                 </ul>
+//             </nav>
+//             <Outlet/>
+//         </div>
+//     );
+// };
+//
+// const routes = [
+//     {
+//         path: '/',
+//         element: <MainLayout/>,
+//         children: [
+//             {path: 'lazy1', element: <LazyPage1/>},
+//             {path: 'lazy2', element: <LazyPage2/>},
+//             {path: 'lazy3', element: <LazyPage3/>},
+//             {path: '*', element: <Navigate to="/lazy1" replace/>},
+//         ],
+//     },
+// ];
+//
+// const instrumentedRouter = withFaroRouterInstrumentation(createBrowserRouter(routes));
 
-const MainLayout = () => {
-    const activeNavStyle = ({isActive}: NavLinkRenderProps) => isActive ? 'nav-active' : '';
+const Navigation = () => {
+    const activeNavStyle = ({isActive}: NavLinkRenderProps) => {
+        return isActive ? 'nav-active' : '';
+    }
     return (
-        <div className="main-layout">
-            <nav>
-                <img src={logo} alt="React Logo"/>
-                <ul>
-                    <li>
-                        <NavLink to="lazy1" className={activeNavStyle}>Lazy 1</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="lazy2" className={activeNavStyle}>Lazy 2</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="lazy3" className={activeNavStyle}>Lazy 3</NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet/>
-        </div>
+        <Suspense fallback={<span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={logo} alt="React Logo"/>
+                        <ul>
+                            {
+                                routes.map(({to, name}) => (
+                                    <li key={to}>
+                                        <NavLink to={to} className={activeNavStyle}>{name}</NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                    <Routes>
+                        {routes.map(({path, Component}) => (
+                            <Route key={path} path={path} element={<Component/>}/>
+                        ))
+                        }
+                        <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     );
 };
 
-const routes = [
-    {
-        path: '/',
-        element: <MainLayout/>,
-        children: [
-            {path: 'lazy1', element: <LazyPage1/>},
-            {path: 'lazy2', element: <LazyPage2/>},
-            {path: 'lazy3', element: <LazyPage3/>},
-            {path: '*', element: <Navigate to="/lazy1" replace/>},
-        ],
-    },
-];
-
-const instrumentedRouter = withFaroRouterInstrumentation(createBrowserRouter(routes));
-
-// const Navigation = () => {
-//     const activeNavStyle = ({isActive}: NavLinkRenderProps) => {
-//         return isActive ? 'nav-active' : '';
-//     }
-//     return (
-//         <BrowserRouter>
-//             <div className="main-layout">
-//                 <nav>
-//                     <img src={logo} alt="React Logo"/>
-//                     <ul>
-//                         <li>
-//                             <NavLink to="/home" className={activeNavStyle}>Home</NavLink>
-//                         </li>
-//                         <li>
-//                             <NavLink to="/about" className={activeNavStyle}>About</NavLink>
-//                         </li>
-//                         <li>
-//                             <NavLink to="/users" className={activeNavStyle}>Users</NavLink>
-//                         </li>
-//                     </ul>
-//                 </nav>
-//                 <Routes>
-//                     <Route path="about" element={<h1>About Page</h1>}/>
-//                     <Route path="users" element={<h1>Users Page</h1>}/>
-//                     <Route path="home" element={<h1>Home Page</h1>}/>
-//
-//                     <Route path="/*" element={<Navigate to="/home" replace/>}/>
-//                 </Routes>
-//             </div>
-//         </BrowserRouter>
-//     );
-// };
-
-const Navigation = () => <RouterProvider router={instrumentedRouter}/>;
+// const Navigation = () => <RouterProvider router={instrumentedRouter}/>;
 
 export default Navigation;
